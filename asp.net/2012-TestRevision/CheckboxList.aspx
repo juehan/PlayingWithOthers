5c8d803e77254a0e11fe05d56952e8294d3a5c7d
John Lee <to.johnny.lee@gmail.com>
2012-05-16 21:35:37 +1000

    initial check in

---
+++ asp.net/2012-TestRevision/CheckboxList.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CheckboxList.aspx.cs" Inherits="beforeTest.checkboxlist" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="StyleSheet.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div id="Page">
+        <asp:Label ID="Label1" runat="server" Text="Select the days you attend TAFE:"></asp:Label><br />
+        <asp:CheckBoxList ID="CheckBoxList1" runat="server">
+            <asp:ListItem>Monday</asp:ListItem>
+            <asp:ListItem>Tuesday</asp:ListItem>
+            <asp:ListItem>Wednesday</asp:ListItem>
+            <asp:ListItem>Thursday</asp:ListItem>
+            <asp:ListItem>Friday</asp:ListItem>
+        </asp:CheckBoxList><br />
+
+        <asp:Button ID="Button1" runat="server" Text="Days" onclick="Button1_Click" /><br /><br />
+        <asp:Label ID="lblOutput" runat="server" Text=""></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ asp.net/2012-TestRevision/MagazineSubscription.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace beforeTest
+{
+    public partial class MagazineSubscription : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Wizard1_FinishButtonClick(object sender, WizardNavigationEventArgs e)
+        {
+            string strMagazines=string.Empty;
+
+            foreach (ListItem li in ChksubscriptList.Items)
+            {
+                if(li.Selected)
+                    strMagazines +=li.Text + "<br/>";
+            }
+
+            lblFinished.Text = string.Format("Thank you {0} {1} <br/> Your details are: <br/>" +
+                                            "Phone:{2} <br/> Email:{3} <br/> Address:{4} <br/>" +
+                                            "Lengh Of subscription:{5} <br/> Subscription(s):<br/>{6}",  
+                                            txtFirstName.Text, txtLastName.Text, 
+                                            txtContactNumber.Text, txtEmail.Text, txtAddress.Text,
+                                            DropDownList1.SelectedItem.Text, strMagazines);
+        }
+    }
+}
