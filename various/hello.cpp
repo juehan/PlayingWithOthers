2741d79d9ae272d0119eea9414f612d9ffce5bb4
John.L <to.johnny.lee@gmail.com>
2012-12-13 18:13:05 +1100

    Update various/hello.cpp

--- various/hello.cpp
+++ various/hello.cpp
@@ -2,6 +2,14 @@
 
 using namespace std;
 
+tempalte <typename T>
+class Foo{
+  T data;
+  public:
+    T get(){return data;}
+    void set(T t){ data = t;}
+};
+
 int main()
 {
   cout<<"hello world"<<endl;
